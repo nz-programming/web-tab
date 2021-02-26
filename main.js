c18a37a0d3c3539110a9eeea200d7b09dbec8410
Hiroki <s.s.ka.tu.ok@gmail.com>
2021-02-26 18:07:27 +0900

    不要なconsole.log削除

--- main.js
+++ main.js
@@ -4,23 +4,18 @@ const showTab = (selector) => {
     
     //コンテンツの非表示
     $('.tabs-content > div').hide();
-    console.log(selector);
     
     //activeクラスの付与
     $(`.tabs-menu div#${selector}`)
     .addClass('active');
-    console.log(`.tabs-menu div#${selector}`);
     
     //activeクラスに紐づくコンテンツの表示
     const endfigure = selector.slice(-1);
-    console.log(selector.slice(-1));
     $(`div #tabs-${endfigure}`).show();
 };
 
 $('.tabs-menu div').on('click', (e) => {
     const selector = $(e.target).attr('id');
-    console.log(e);
-    console.log(selector);
     showTab(selector);
 });
 
